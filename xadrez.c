ef1b46295c90f2a45d86d57577b6c8d0511e6df3
Josué Ferreira <69156243+JohnWick92@users.noreply.github.com>
2025-06-02 16:35:14 +0000

    level_two

--- xadrez.c
+++ xadrez.c
@@ -1,25 +1,55 @@
 #include <stdio.h>
+#define ROOK_MOVES 5
+#define BISHOP_MOVES 5
+#define QUEEN_MOVES 8
+#define KNIGHT_DOUBLE 2
+#define KNIGHT_SINGLE 1
 
 // Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
+// Este código inicial serve como base para o desenvolvimento do sistema de movimentaçãocount das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+    int count = 0;
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+    
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    for(count = 0; count < BISHOP_MOVES; count++) {
+        printf("Cima Direita\n");
+    }
+    printf("\n");
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    count = 0;
+    do
+    {
+        printf("Direita\n");
+        count++;
+    } while (count < ROOK_MOVES);
+    printf("\n");
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    count = 0;
+    while (count < QUEEN_MOVES) {
+        count++;
+        printf("Esquerda\n");
+    }
+    printf("\n");
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    count = 0;
+    while (count < KNIGHT_SINGLE) {
+        for (count; count < KNIGHT_DOUBLE; count++) {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
